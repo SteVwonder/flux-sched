5014343364d0461970a434110c81678272c032d9
Stephen Herbein <sherbein@udel.edu>
2016-01-14 14:10:03 -0800

    hype-io conf now compatible with IO-Aware scheduler

--- conf/hype-io.lua
+++ conf/hype-io.lua
@@ -3,16 +3,15 @@ uses "Node"
 
 Hierarchy "default"
 {
-   Resource{ "cluster", name = "hype", io=10000,
-             children = { ListOf{ Node,
-                                  ids = "201-354",
-                                  args = {
-                                     name = "hype",
-                                     sockets = {"0-7", "8-15"},
-                                     memory_per_socket = 15000,
-                                     io = 1440
-                                  }
-                                }
-             }
-   }
+   Resource{ "filesystem", name="pfs", io = 48000, children = {
+                ListOf{ Node,
+                        ids = "201-354",
+                        args = {
+                           name = "hype",
+                           sockets = {"0-7", "8-15"},
+                           memory_per_socket = 15000,
+                           io = 1440
+                }}
+   }}
 }
+
