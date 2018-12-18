f306616cb9ba140cee1ad90742c97a7405f2d4fb
Stephen Herbein <stephen272@gmail.com>
2018-12-17 21:23:08 -0800

    increase queue depth

--- sched/scheduler.h
+++ sched/scheduler.h
@@ -95,7 +95,7 @@ typedef struct {
  * new values also need to be passed down to the scheduling
  * plug-ins.
  */
-#define SCHED_PARAM_Q_DEPTH_DEFAULT 1024
+#define SCHED_PARAM_Q_DEPTH_DEFAULT 8192
 
 const sched_params_t *sched_params_get (flux_t h);
 
